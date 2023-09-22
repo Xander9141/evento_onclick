dacddb9c291aa86dc343316fc8ffce97f6ed61d5
Xander9141 <ramontorres91.00@gmail.com>
2023-09-22 12:29:38 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+function cambiarLogin(elemento) {
+    if (elemento.innerText == "Login") {
+        elemento.innerText = "Logout";
+    } else {
+        elemento.innerText = "Login"
+    }
+}
+
+function alertar(elemento) {
+    alert("Ninja was liked");
+}
+
+
+function ocultarBoton(elemento) {
+    elemento.style.display = 'none';
+}
+
+
+
+
+
